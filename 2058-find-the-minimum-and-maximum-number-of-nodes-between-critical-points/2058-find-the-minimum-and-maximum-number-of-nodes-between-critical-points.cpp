206ca2e5333e6b97d289e5b5023f2f3d66b03d03
DevVratSharma911 <devvratsharma101@gmail.com>
2026-08-31 18:29:09 +0530

    Time: 0 ms (100%), Space: 124.6 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,49 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        ListNode *prev=head;
-        
-        ListNode *temp=head->next;
-        
-        
-        
-        int count=1;
-        
-        vector<int>values;
-        
-        while(temp->next!=NULL){
-            if(temp->val>prev->val && temp->val>temp->next->val){
-                values.push_back(count);
+        ListNode* prev = head;
+        ListNode* temp = head->next;
+
+        int count = 1;
+
+        int first = -1;
+        int last = -1;
+        int minDis = INT_MAX;
+
+        while (temp->next != NULL) {
+
+            if ((temp->val > prev->val && temp->val > temp->next->val) ||
+                (temp->val < prev->val && temp->val < temp->next->val)) {
+
+                if (first == -1) {
+                    first = count;
+                    last = count;
+                }
+                else {
+                    minDis = min(minDis, count - last);
+                    last = count;
+                }
             }
-            if(temp->val<prev->val && temp->val<temp->next->val){
-                values.push_back(count);
-            }
-            temp=temp->next;
-            prev=prev->next;
+
+            temp = temp->next;
+            prev = prev->next;
             count++;
-            
         }
-        if(values.size()<2)return{-1, -1};
-        int min_dis=INT_MAX;
-        for(int i=1;i<values.size();i++){
-            if(values[i]-values[i-1]<min_dis){
-                min_dis=values[i]-values[i-1];
-            }
-            
-            
-            
-       }
-       return{min_dis, values[values.size()-1]-values[0]};
-        
+
+        if (first == -1 || first == last)
+            return {-1, -1};
+
+        return {minDis, last - first};
     }
 };
